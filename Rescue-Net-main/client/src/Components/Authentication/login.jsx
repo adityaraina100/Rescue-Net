d83c166da62f857534ccf2113a4226987863272a
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-12-12 23:51:56 +0530

    Update login.jsx

--- Rescue-Net-main/client/src/Components/Authentication/login.jsx
+++ Rescue-Net-main/client/src/Components/Authentication/login.jsx
@@ -1,31 +1,45 @@
 import React, { useState } from 'react';
-
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { useToast } from '@chakra-ui/react'; 
 const LoginPage = () => {
-  const [username, setUsername] = useState('');
+  const [email, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = async () => {
+  const navigate = useNavigate();
+  const toast = useToast(); // Initialize useToast
+  const handleLogin = async (e) => {
+    e.preventDefault();
     try {
-      const response = await fetch('http://localhost:3001/login', {
-        method: 'POST',
+      const config = {
         headers: {
-          'Content-Type': 'application/json',
+          "Content-type": "application/json",
         },
-        body: JSON.stringify({
-          username,
-          password,
-        }),
+      };
+      let { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+      console.log(data)
+      localStorage.setItem("RescueInfo", JSON.stringify(data));
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 2000, 
+        isClosable: true,
+        position: "bottom",
       });
-
-      if (response.ok) {
-        // Handle successful login
-        console.log('Login successful');
-      } else {
-        // Handle failed login
-        console.error('Login failed');
-      }
+      navigate("/home");
     } catch (error) {
-      console.error('Error during login:', error);
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
     }
   };
 
@@ -38,12 +52,12 @@ const LoginPage = () => {
         <div className="flex items-center">
           <input
             type="text"
-            id="username"
+            id="email"
             placeholder="Enter your email"
-            name="username"
-            value={username}
+            name="email"
+            value={email}
             onChange={(e) => setUsername(e.target.value)}
-            className="h-14 p-4 rounded-lg border border-gray-300 mb-4"
+            className="h-14 p-4 rounded-lg border border-gray-300 mb-4 m-10"
             required
           />
           <input
@@ -53,7 +67,7 @@ const LoginPage = () => {
             name="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="h-14 p-4 rounded-lg border border-gray-300 mb-4"
+            className="h-14 p-4 rounded-lg border border-gray-300 mb-4 m-10"
             required
           />
         </div>
@@ -63,7 +77,7 @@ const LoginPage = () => {
         </button>
 
         <button className="w-full h-14 bg-gray-300 text-gray-700 rounded-lg">
-          Signup
+        <Link to="/signup">Signup</Link>
         </button>
       </form>
     </div>
